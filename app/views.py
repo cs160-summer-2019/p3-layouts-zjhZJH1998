c13052f46d22a643c9e530cf54ca2e072861c06d
Andrew Head <head.andrewm@gmail.com>
2019-05-24 17:49:52 -0700

    update to reactive layouts prototype code

--- app/views.py
+++ app/views.py
@@ -1,12 +1,29 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
 
 def a(request):
-    return render(request, 'a.html')
+	print("hi")
+	return render(request, 'a.html')
 
 def b(request):
-    return render(request, 'b.html')
+	return render(request, 'b.html')
 
-def index(request):
-    return render(request, 'index.html')
+def c(request):
+	return render(request, 'c.html')
+
+def d(request):
+	return render(request, 'd.html')
+
+def e(request):
+	return render(request, 'e.html')
+
+def f(request):
+	return render(request, 'f.html')
+
+def g(request):
+	return render(request, 'g.html')
+
+def h(request):
+	return render(request, 'h.html')
+
+def index(request): 
+	return render(request, "index.html")
